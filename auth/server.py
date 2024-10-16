07eee01da74208e55f996a4a8f2b5c73f009a4a5
Lucas Stoerk <lucas@zocki.lan>
2024-10-16 17:10:56 +0200

    fix auth

--- auth/server.py
+++ auth/server.py
@@ -8,23 +8,22 @@ mysql = MySQL(server)
 # config
 server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
 server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
-server.config["MYSQL_PW"]   = os.environ.get("MYSQL_PW")
-server.config["MYSQL_DB"]   = os.environ.get("MYSQL_DB")
-server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT"))
 
-# routes
-## login
+# Routes
+## Login
 @server.route("/login", methods=["POST"])
 def login():
     auth = request.authorization
-    if not auth: 
+    if not auth:
         return "missing credentials", 401
 
-    # check db for user + pw
+    # check db for username and password
     cur = mysql.connection.cursor()
     res = cur.execute(
         "SELECT email, password FROM user WHERE email=%s", (auth.username,)
-
     )
 
     if res > 0:
@@ -32,45 +31,46 @@ def login():
         email = user_row[0]
         password = user_row[1]
 
-        if auth.username != email or auth.password != password: 
+        if auth.username != email or auth.password != password:
             return "invalid credentials", 401
         else:
-            return createJWT(auth.username, os.environment.get("JWT_SECRET"), True)
-    else: 
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    else:
         return "invalid credentials", 401
-    
-## validate
+
+## Validate
 @server.route("/validate", methods=["POST"])
 def validate():
     encoded_jwt = request.headers["Authorization"]
 
     if not encoded_jwt:
         return "missing credentials", 401
-    
+
     encoded_jwt = encoded_jwt.split(" ")[1]
 
     try:
         decoded = jwt.decode(
-            encoded_jwt, os.environment.get("JWT_SECRET"), algorithm=["HS256"]
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
         )
-    except: 
+    except:
         return "not authorized", 403
-    
+
     return decoded, 200
 
-# JWT function to create auth token
+
 def createJWT(username, secret, authz):
     return jwt.encode(
         {
             "username": username,
-            "exp": datetime.datetime.now(datetime.timezone.utc) 
-            + datetime.timedelta(1),
-            "iat": datetime.datetime.now(datetime.timezone.utc),
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+            + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
             "admin": authz
         },
         secret,
         algorithm="HS256"
     )
+
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=5000)
